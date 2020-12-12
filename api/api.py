341ea03c833b22293b75478744d9cf965c82be30
Henok Tilaye <henoktilaye7@gmail.com>
2020-12-12 16:43:22 +0300

    first skeleton

--- api/api.py
+++ api/api.py
@@ -1,8 +1,18 @@
-import time
 from flask import Flask
 
 app = Flask(__name__)
 
+# Features to visualize on the scale of 0 to 10
+data_points = [
+	{ "name": "likes", "value": 8.7 },
+	{ "name": "CVR", "value": 6 },
+	{ "name": "CTR", "value": 8 },
+	{ "name": "KPI", "value": 7.567 }
+];
+	
+
 @app.route('/time')
 def get_current_time():
-    return {'time': time.time()}
+    return {
+    	'data': data_points
+    	}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function BarGroup(props) {
+  let barPadding = 2
+  let barColour = '#348AA7'
+  let widthScale = d => d * 10
+
+  let width = widthScale(props.d.value)
+  let yMid = props.barHeight * 0.5
+  
+  return <g className="bar-group">
+    <text className="name-label" x="-6" y={yMid} alignmentBaseline="middle" >{props.d.name}</text>
+    <rect y={barPadding * 0.5} width={width} height={props.barHeight - barPadding} fill={barColour} />
+    <text className="value-label" x={width- 8} y={yMid} alignmentBaseline="middle" >{props.d.value}</text>
+  </g>
 }
+
+class App extends React.Component {
+  state = {
+    clicked: false,
+    data: [],
+  }
+
+  clickHandler() {
+    fetch('/time').then(res => res.json()).then(data => {
+      this.setState({"data": data.data});
+    });
+    this.setState({'clicked': true});
+  }
+
+  render() {
+    
+    let barHeight = 30
+        
+    let barGroups = this.state.data.map((d, i) => <g transform={`translate(0, ${i * barHeight})`}>
+                                                    <BarGroup d={d} barHeight={barHeight} />
+                                                  </g>)                         
+    
+     
+    if(this.state.clicked) {  
+        return (
+          <svg width="800" height="300" >
+
+            <g className="container">
+              <text className="title" x="10" y="30">Features</text>
+              <g className="chart" transform="translate(100,60)">
+                {barGroups}
+              </g>
+            </g>
+
+          </svg>
+        );
+      }
+    else {
+        return (
+          <form ref={"form"} onSubmit={()=>this.clickHandler()}>
+            <button type="submit">Show me the features</button>
+          </form>
+          );
+      }
+  }
+}
+
 
 export default App;
