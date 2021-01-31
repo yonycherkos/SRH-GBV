ee2500b707de1eb2b697dcd98f17a14259decc24
yonycherkos <yonycherkos@gmail.com>
2021-01-31 12:13:35 +0300

    barchart to visualize woe and iv of a give fb post

--- src/App.js
+++ src/App.js
@@ -1,39 +1,47 @@
-import React, { PureComponent, Fragment } from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Fragment } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import {
-  BarChart, 
-  Bar, 
-  Cell, 
-  XAxis, 
-  YAxis, 
-  CartesianGrid, 
-  Tooltip, 
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
   Legend,
-} from 'recharts';
+} from "recharts";
 
-import { 
-  Navbar, 
-  Nav, 
-  NavDropdown, 
-  Form, 
-  FormControl, 
-  Button 
-} from 'react-bootstrap';
+import {
+  Navbar,
+  Nav,
+  Form,
+  Button,
+} from "react-bootstrap";
 
 class App extends React.Component {
   state = {
     clicked: false,
+    pageName: "",
     data: [],
+  };
+
+  static jsfiddleUrl = "https://jsfiddle.net/alidingling/30763kr7/";
+
+  handleChange = (event) => {
+    this.setState({ pageName: event.target.value });
   }
 
-  static jsfiddleUrl = 'https://jsfiddle.net/alidingling/30763kr7/';
-
-  clickHandler() {
-    fetch('/time').then(res => res.json()).then(data => {
-      this.setState({"data": data.data});
-    });
-    this.setState({'clicked': true});
+  handleSubmit = (event) => {
+    event.preventDefault();
+    fetch("/api", {
+      method: "POST", 
+      headers: {"Content-Type":"application/json"},
+      body: JSON.stringify({"pageName": this.state.pageName})
+    }).then((response) => response.json())
+      .then((response) => {
+        this.setState({ data: response.data });
+      }).catch((error) => console.log(error));
+    this.setState({ clicked: true });
   }
 
   render() {
@@ -44,63 +52,50 @@ class App extends React.Component {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link 
-                href="#home" 
-                onClick={() => this.setState({'clicked': false})}
-                >
-                  Home
+              <Nav.Link
+                href="#home"
+                onClick={() => this.setState({ clicked: false })}
+              >
+                Home
               </Nav.Link>
             </Nav>
-              <Button 
-                variant="outline-success" 
-                onClick={() => this.clickHandler()}
-                >
-                Visualize
-              </Button>
           </Navbar.Collapse>
         </Navbar>
-        <div className={'description'}>
+        <div className={"description"}>
           <p>
-            This simple tool helps to visualize important features for a SRH/GBV digital content.
-            <br/>
+            This simple tool helps to visualize important features for a SRH/GBV
+            digital content.
+            <br />
             It takes the Link as input and displays the features.
           </p>
-          </div>
-          <div className={'description'}>
-          <Form inline>
-            <FormControl type="text" placeholder="Link" onSubmit={() => this.s} />
-            <Button variant="primary">Visualize</Button>
+        </div>
+        <div className={"description"}>
+          <Form inline onSubmit={this.handleSubmit}>
+            <input
+              name="page_name"
+              placeholder="page name"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+            <Button type="submit" variant="primary">Visualize</Button>
           </Form>
-          </div>
-        {
-          this.state.clicked ?
+        </div>
+        {this.state.clicked ? (
           <div className={"barChart"}>
-          <BarChart
-            
-            width={800}
-            height={500}
-            data={this.state.data}
-            margin={{
-              top: 5, right: 30, left: 20, bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="value" fill="#0088A9" />
-            
-          </BarChart>
+            <BarChart width={730} height={250} data={this.state.data}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="keyword" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="woe" fill="#8884d8" />
+              <Bar dataKey="iv" fill="#82ca9d" />
+            </BarChart>
           </div>
-          :
-          null
-          }
+        ) : null}
       </Fragment>
-      );
-  
+    );
   }
 }
-
 
 export default App;
