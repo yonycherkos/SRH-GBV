06d5852e50e8772779317edd77f71f8698988a85
yonycherkos <yonycherkos@gmail.com>
2021-02-05 22:05:46 +0300

    build functions to extract woe and iv

--- api/api.py
+++ api/api.py
@@ -8,35 +8,81 @@ app = Flask(__name__)
 
 # get specific facebook post data from the given url
 def fb_scraper(page_name):
-	post_txt = ''.join([post['text'] for post in get_posts(page_name, pages=1)])
+	post_txt = ''.join([post["text"] for post in get_posts(page_name, pages=1)])
 	return post_txt
 
 # convert the post data into keyword lists
-def text_to_keywords(txt):
-	f = open('../data/keywords.json')
-	keywords = json.load(f)
-	keyword_list = []
-	for list_ in keywords.values():
-		keyword_list.extend(list_)
+def text_to_contents(txt):
+	f = open("../data/sub_topics.json")
+	sub_topics = json.load(f)
+	f.close()
+	quantieles = [281.0, 674.25]
+	contents = {}
+	for (sub_topic_name, content_list) in sub_topics.items():
+		if sub_topic_name == "content_size":
+			if len(txt) <= quantieles[0]:
+				contents[sub_topic_name].append("short")
+			elif len(txt) >= quantieles[1]:
+				contents[sub_topic_name].append("long")
+			else:
+				contents[sub_topic_name].append("meduim")
+		else:
+			for content in content_list:
+				if content.lower() in str(txt).lower():
+					contents[sub_topic_name].append(content)
+	return contents
 
-	keywords = list(set([str(keyword) for keyword in keyword_list if (keyword.lower() in str(txt).lower())]))
-	return keywords
+# load iv data
+def load_data():
+	iv_df= pd.read_csv("../data/iv_data.csv", index_col=0)
+	woe_df = pd.read_csv("../data/woe_data.csv", index_col=0)
 
-# extract associated woe and iv
-def keywords_to_woe_and_iv(keywords):
-	woe_iv_data = pd.read_csv('../data/woe_iv_data.csv', index_col='keyword')
-	woe = list(woe_iv_data.loc[keywords]['woe'])
-	iv = list(woe_iv_data.loc[keywords]['iv'])
-	return (woe, iv)
+	iv_data = {}
+	iv_data["sub_topic_names"] = list(iv_df.index)
+	iv_data["iv"] = list(iv_df["iv"])
+	iv_data["iv_interpretation"] = list(iv_df["iv_interpretation"])
 
-@app.route('/api', methods=['POST'])
+	woe_data = {}
+	woe_data["contents"] = list(woe_df.index)
+	woe_data["sub_topics"] = list(woe_df["sub_topics"])
+	woe_data["woe"] = list(woe_df["woe"])
+
+	return (iv_data, woe_data)
+
+# extract iv from the post contents
+def extract_iv(contents):
+	iv_df= pd.read_csv("../data/iv_data.csv", index_col=0)
+	sub_topic_names = list(contents.keys())
+
+	iv_data = {}
+	iv_data["sub_topic_names"] = sub_topic_names
+	iv_data["iv"] = list(iv_data.loc[sub_topic_names, "iv"])
+	iv_data["iv_interpretation"] = list(iv_data.loc[sub_topic_names, "iv_interpretation"])
+
+	return iv_data
+
+# extract woe from the post contents
+def extract_woe(contents):
+	woe_df = pd.read_csv("../data/woe_data.csv", index_col=0)
+	content_list = list(contents.values())
+
+	woe_data = {}
+	woe_data["contents"] = list(woe_df.loc[content_list].index)
+	woe_data["sub_topics"] = list(woe_df.loc[content_list, "sub_topics"])
+	woe_data["woe"] = list(woe_df.loc[content_list, "woe"])
+
+@app.route("/data", methods=['POST'])
+def home():
+	(iv_data, woe_data) = load_data()
+	return jsonify({"iv_data": iv_data, "woe_data": woe_data})
+
+@app.route("/api", methods=['POST'])
 def get_visualization_data():
 	request_data = request.get_json()
 	txt = fb_scraper(request_data['pageName'])
-	keywords = text_to_keywords(txt)
-	(woe_, iv_) = keywords_to_woe_and_iv(keywords)
 
-	data = []
-	for (keyword, woe, iv) in zip(keywords, woe_, iv_):
-		data.append({'keyword': keyword, 'woe': woe, 'iv': iv})
-	return jsonify({'data': data})
+	contents = text_to_contents(txt)
+	iv_data = extract_iv(contents)
+	woe_data = extract_woe(contents)
+
+	return jsonify({"iv_data": iv_data, "woe_data": woe_data})
