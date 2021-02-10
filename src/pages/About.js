bd5a845d6a138787ac59dc0ab019b1139b2bbc49
yonycherkos <yonycherkos@gmail.com>
2021-02-10 10:51:14 +0300

    build about page.

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = (props) => {
+  return (
+    <div className="main-content">
+      <div className="card intro">
+        This is a feature visualization tool for digital content related to the
+        topics of SRH and GBV. This tool includes data from different digital
+        platforms(social media accounts, groups, pages, facebook, twitter,
+        websites both developed locally and internationally) with a core focus
+        on SRH & GBV. This visualization tool visualizes the features that
+        determine the importance of SRH and GBV content.
+      </div>
+    </div>
+  );
+};
+
+export default About;
