616ad6bec528e45b8d8f171001d24566fe263910
yonycherkos <yonycherkos@gmail.com>
2021-02-13 18:12:01 +0300

    increase number of nav items

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,30 +1,31 @@
-import React from 'react';
-import {
-  Navbar,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink} from 'reactstrap';
+import React from "react";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
 
- export default class Navigation extends React.Component {
- 	render(){
- 		return(
-     	<div className="App-header">
-            <Navbar color="faded" light expand="md">
-                <NavbarBrand>GBV-SRH</NavbarBrand>
-                <Nav className="ml-auto" navbar>
-                  <NavItem>
-                    <NavLink href="/">Home</NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink href="/visualize">Visualize</NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink href="/about">About</NavLink>
-                  </NavItem>
-                </Nav>
-            </Navbar>
-        </div>
- 		);
- 	}
- }
+export default class Navigation extends React.Component {
+  render() {
+    return (
+      <div className="App-header">
+        <Navbar color="faded" light expand="md">
+          <NavbarBrand>GBV-SRH</NavbarBrand>
+          <Nav navbar className="ml-auto">
+            <NavItem>
+              <NavLink href="/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/SRH">SRH</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/GBV">GBV</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/visualize">Visualize</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/about">About</NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar>
+      </div>
+    );
+  }
+}
