59636588a3f6774f95da65687070f4c9e74faf09
Tewodros <teddylnk1@gmail.com>
2021-02-14 16:06:36 +0300

    initial commit for heroku

--- api/api.py
+++ api/api.py
@@ -3,8 +3,9 @@ from facebook_scraper import get_posts
 import pandas as pd
 import requests
 import json
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../build", static_url_path="/")
 
 
 def extract_home_data():
@@ -188,6 +189,16 @@ def extract_GBV_post_data(contents):
     return (GBV_iv_data, GBV_woe_data)
 
 
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
 @app.route("/home")
 def load_home_data():
     (SRH_iv_data, GBV_iv_data) = extract_home_data()
@@ -231,3 +242,7 @@ def load_post_data():
                         "GBV_iv_data": GBV_iv_data, "GBV_woe_data": GBV_woe_data, })
     return jsonify({"SRH_iv_data": None, "SRH_woe_data": None,
                     "GBV_iv_data": None, "GBV_woe_data": None, })
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
