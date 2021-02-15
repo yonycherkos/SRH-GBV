e45c5e15d6e6eb2667f6409d38f89498d140743b
yonycherkos <yonycherkos@gmail.com>
2021-02-15 18:47:18 +0300

    change route path for deployment

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ class App extends Component {
       <div className="wrapper">
         <Navigation />
         <Switch>
-          <Route exact path="/" render={(props) => <Home {...props} />} />
-          <Route exact path="/home" render={(props) => <Home {...props} />} />
-          <Route exact path="/SRH" render={(props) => <SRH {...props} />} />
-		      <Route exact path="/GBV" render={(props) => <GBV {...props} />} />
-          <Route exact path="/visualize" render={(props) => <Visualize {...props} />}/>
-          <Route exact path="/about" render={(props) => <About {...props} />} />
+          <Route exact path="http://srh-flask.herokuapp.com/api/home" render={(props) => <Home {...props} />} />
+          <Route exact path="http://srh-flask.herokuapp.com/api/home" render={(props) => <Home {...props} />} />
+          <Route exact path="http://srh-flask.herokuapp.com/api/SRH" render={(props) => <SRH {...props} />} />
+		      <Route exact path="http://srh-flask.herokuapp.com/api/GBV" render={(props) => <GBV {...props} />} />
+          <Route exact path="http://srh-flask.herokuapp.com/api/visualize" render={(props) => <Visualize {...props} />}/>
+          <Route exact path="http://srh-flask.herokuapp.com/api/about" render={(props) => <About {...props} />} />
         </Switch>
       </div>
     );
